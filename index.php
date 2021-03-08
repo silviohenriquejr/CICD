cfb3e412575defe890c0d7db23ce4fcd25fd9d71
SilvioHenrique <silviohenriquejr@gmail.com>
2021-03-08 14:38:10 -0300

    Correção apontamento Assets Template BootStrap

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dewi Bootstrap Template - Index</title>
+  <title>Testes CICD TP Bootstrap - Index</title>
   <meta name="robots" content="noindex, nofollow">
   <meta content="" name="description">
   <meta content="" name="keywords">
@@ -44,16 +44,16 @@
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.html">Dewi</a></h1>
+      <h1 class="logo"><a href="index.html">Turma 15B - ITAU & Gama Academy</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="https://bootstrapmade.com/demo/templates/Dewi/assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto active" href="#hero">HOME</a></li>
+          <li><a class="nav-link scrollto" href="#about">JAVA</a></li>
+          <li><a class="nav-link scrollto" href="#services">GIT</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">HEROKU</a></li>
           <li><a class="nav-link scrollto" href="#team">Team</a></li>
           <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
@@ -72,8 +72,8 @@
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+          <li><a class="nav-link scrollto" href="#contact">CONTATO</a></li>
+          <li><a class="getstarted scrollto" href="#about">COMEÇAR</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -84,11 +84,11 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero">
     <div class="hero-container" data-aos="fade-up" data-aos-delay="150">
-      <h1>Plan. Launch. Grow.</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      <h1>Bem Vindo ao Programa de Desenvolvimento DOTI</h1>
+      <h2>Acompanhe aqui a evolução da Turma 15B!</h2>
       <div class="d-flex">
         <a href="#about" class="btn-get-started scrollto">Get Started</a>
-        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+        <a href="zoom.us/j/96629485591" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Acompanhe nossas aulas em tempo real</span></a>
       </div>
     </div>
   </section><!-- End Hero -->
