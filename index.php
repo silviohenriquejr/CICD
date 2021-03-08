af4b4de09afea360f1ede6062006d368caf6f8c8
SilvioHenrique <silviohenriquejr@gmail.com>
2021-03-08 14:49:11 -0300

    Ajustes Template BootStrap

--- index.php
+++ index.php
@@ -54,23 +54,6 @@
           <li><a class="nav-link scrollto" href="#about">JAVA</a></li>
           <li><a class="nav-link scrollto" href="#services">GIT</a></li>
           <li><a class="nav-link scrollto " href="#portfolio">HEROKU</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
           <li><a class="nav-link scrollto" href="#contact">CONTATO</a></li>
           <li><a class="getstarted scrollto" href="#about">COMEÇAR</a></li>
         </ul>
